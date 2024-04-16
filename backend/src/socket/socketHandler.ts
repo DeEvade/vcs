461e9b9359686bcc35c01a142d55dac01a30b7e3
William Redgård <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>
2024-04-16 14:09:28 +0200

    fixed socket

--- backend/src/socket/socketHandler.ts
+++ backend/src/socket/socketHandler.ts
@@ -2,22 +2,19 @@ import { Server, Socket } from "socket.io";
 import { Configuration } from "../database/entities/Configuration";
 import { DataSource } from "typeorm";
 
+const socketHandler = (socket: Socket, AppDataSource: DataSource) => {
+  socket.on("getConfig", async () => {
+    console.log("asking for config");
 
-const socketHandler = (io: Server,AppDataSource: DataSource ) => {
-
-    io.on("getConfig", async (socket: Socket) => {
-       try{
-         const configRepository = AppDataSource.getRepository(Configuration)
-        const config = await configRepository.find()
-        socket.emit('configdata', config[1])
+    try {
+      const configRepository = AppDataSource.getRepository(Configuration);
+      const config = await configRepository.find();
+      socket.emit("configdata", config[1]);
+    } catch (error) {
+      console.log("Error during default configuration creation", error);
+      socket.emit("error", "Failed to fetch configuration data");
     }
-    catch (error) {
-        console.log("Error during default configuration creation", error); 
-        socket.emit("error", "Failed to fetch configuration data");
-
-    }
-        
-    });
-}
+  });
+};
 
 export default socketHandler;
