e8f95e1338d7ce936e0e163c5e8137db1c3d2e21
William Redgård <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>
2024-04-16 11:34:13 +0200

    dummy data

--- backend/src/database/createDefaultConfig.ts
+++ backend/src/database/createDefaultConfig.ts
@@ -4,32 +4,66 @@ import { Role } from "./entities/Role";
 import { Frequency } from "./entities/Frequency";
 import { RoleFrequency } from "./entities/RoleFrequency";
 
-
 export default async function createDefaultConfig(dataSource: DataSource) {
-    const configRepo = dataSource.getRepository(Configuration);
-    const roleRepo = dataSource.getRepository(Role);
-    const frequencyRepo = dataSource.getRepository(Frequency);
-    const roleFrequencyRepo = dataSource.getRepository(RoleFrequency);
+  const configRepo = dataSource.getRepository(Configuration);
+  const roleRepo = dataSource.getRepository(Role);
+  const frequencyRepo = dataSource.getRepository(Frequency);
+  const roleFrequencyRepo = dataSource.getRepository(RoleFrequency);
 
-    const config = await configRepo.save({ name: 'Default config' });
+  const config = await configRepo.save({ name: "Default config" });
 
+  const ATC1 = await roleRepo.save({
+    name: "OS DIR-E",
+    type: "ATC",
+    configuration: config,
+  });
+  const ATC2 = await roleRepo.save({
+    name: "OS ARR-E",
+    type: "ATC",
+    configuration: config,
+  });
+  const ATC3 = await roleRepo.save({
+    name: "OS APP-C",
+    type: "ATC",
+    configuration: config,
+  });
+  const ATC4 = await roleRepo.save({
+    name: "OS P3",
+    type: "ATC",
+    configuration: config,
+  });
+  const ATC5 = await roleRepo.save({
+    name: "SA TWR W",
+    type: "ATC",
+    configuration: config,
+  });
 
-    const ATC1 = await roleRepo.save({ name: 'OS DIR-E', type: 'ATC', config: config.id })
-    const ATC2 = await roleRepo.save({ name: 'OS ARR-E', type: 'ATC', config: config.id })
-    const ATC3 = await roleRepo.save({ name: 'OS APP-C', type: 'ATC', config: config.id })
-    const ATC4 = await roleRepo.save({ name: 'OS P3', type: 'ATC', config: config.id })
-    const ATC5 = await roleRepo.save({ name: 'SA TWR W', type: 'ATC', config: config.id })
+  const F1 = await frequencyRepo.save({
+    frequency: "118.505",
+    configuration: config,
+  });
+  const F2 = await frequencyRepo.save({
+    frequency: "126.655",
+    configuration: config,
+  });
+  const F3 = await frequencyRepo.save({
+    frequency: "131.130",
+    configuration: config,
+  });
+  const F4 = await frequencyRepo.save({
+    frequency: "120.505",
+    configuration: config,
+  });
 
-    const F1 = await frequencyRepo.save({ frequency: '118.505', config: config.id })
-    const F2 = await frequencyRepo.save({ frequency: '126.655', config: config.id })
-    const F3 = await frequencyRepo.save({ frequency: '131.130', config: config.id })
-    const F4 = await frequencyRepo.save({ frequency: '120.505', config: config.id })
+  await roleFrequencyRepo.save({ role: ATC1, frequency: F4, order: 1 });
+  await roleFrequencyRepo.save({ role: ATC1, frequency: F2, order: 2 });
+  await roleFrequencyRepo.save({ role: ATC1, frequency: F3, order: 3 });
+  await roleFrequencyRepo.save({ role: ATC1, frequency: F1, order: 4 });
 
-    const RFFR1 = await roleFrequencyRepo.save({ role: ATC1, frequency: F4, order: 1 })
-    const RFFR2 = await roleFrequencyRepo.save({ role: ATC2, frequency: F2, order: 2 })
-    const RFFR3 = await roleFrequencyRepo.save({ role: ATC4, frequency: F3, order: 3 })
-    const RFFR4 = await roleFrequencyRepo.save({ role: ATC5, frequency: F1, order: 4 })
-
+  await roleFrequencyRepo.save({ role: ATC1, frequency: F4, order: 1 });
+  await roleFrequencyRepo.save({ role: ATC1, frequency: F2, order: 2 });
+  await roleFrequencyRepo.save({ role: ATC1, frequency: F3, order: 3 });
+  await roleFrequencyRepo.save({ role: ATC1, frequency: F1, order: 4 });
 }
 
 /*
@@ -53,4 +87,4 @@ const initialFrequencies: Frequency[] = [
 ]
 
 
-*/ 
+*/
